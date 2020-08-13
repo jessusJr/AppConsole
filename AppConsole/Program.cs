36b1535cb2419708395082b7855b21c11693928b
Jesus Jimenez Rendon <jesusJimenezRE777@outlook.com>
2020-08-13 17:31:22 -0500

    proyecto en consola

---
+++ AppConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
